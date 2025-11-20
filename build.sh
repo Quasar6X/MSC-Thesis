6f30dd32711a5bf1a0e3cd298d3cbf1419e2f832
Daniel Szucs <daniel.git@pm.me>
2025-11-20 10:25:32 +0100

    Improve build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,30 @@
-#!/bin/sh
+#!/bin/sh -l
 
-readonly WORK_DIR="${1:-$(pwd)}"
+# Constants
 readonly MOUNT_POINT='/workspace'
-readonly USER='ubuntu'
 
-docker run --rm --entrypoint="" -v "$WORK_DIR:$MOUNT_POINT" -w $MOUNT_POINT -u $USER ghcr.io/quasar6x/thesis-builder:latest latexmk
+usage() { echo "Usage: $0 -u <DOCKER_USERNAME> -w <WORK_DIR> [-h]" 1>&2; exit 1; }
+
+unset -v DOCKER_USERNAME WORK_DIR
+while getopts ":u:w:h" opt; do
+    case $opt in
+        u)
+        DOCKER_USERNAME="$OPTARG"
+        ;;
+        w)
+        WORK_DIR="${OPTARG:-$(pwd)}"
+        ;;
+        h|*)
+        usage
+        ;;
+    esac
+done
+
+# Set WORK_DIR if it was not sent on the CLI
+WORK_DIR="${WORK_DIR:-$(pwd)}"
+
+docker run --rm \
+    -v "$WORK_DIR:$MOUNT_POINT" \
+    -w "$MOUNT_POINT" \
+    -u "$DOCKER_USERNAME" \
+    ghcr.io/quasar6x/thesis-builder:latest latexmk
